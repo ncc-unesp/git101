9f920bfc49b8c38374fa5bb3076cf0d3d570b535
Beraldo Leal <beraldo@beraldoleal.com>
2014-08-29 09:40:38 -0300

    added samples dir

---
+++ samples/time_2.c
@@ -0,0 +1,17 @@
+#include <linux/mm.h>
+#include <linux/interrupt.h>
+#include <trace/events/timer.h>
+#include <linux/time.h>
+
+static struct timeval itimer_get_remtime(struct hrtimer *timer)
+{
+  ktime_t rem = hrtimer_get_remaining(timer);
+
+  if (hrtimer_active(timer)) {
+    if (rem.tv64 <= 0)
+      rem.tv64 = NSEC_PER_USEC;
+  } else
+    rem.tv64 = 1;
+
+  return ktime_to_timeval(rem);
+}
